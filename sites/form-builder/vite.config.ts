b049e5df92bd3fe00921cbab26463686cae8be81
Aitor <aitorpalomares@hotmail.com>
2024-02-11 16:49:35 +0100

    ref: migrate moment to dayjs library

--- sites/form-builder/vite.config.ts
+++ sites/form-builder/vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    plugins: [react()],
-    base: './',
-    server: {
-        open: true,
-        port: 8080,
-    },
-});
+  plugins: [react()],
+  base: './',
+  build: {
+    outDir: '../../dist',
+  },
+  server: {
+    open: true,
+    port: 8080,
+  },
+})
